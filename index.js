965168922e88f6e9321515c4b03ea43bf4c561ec
MarlonMoreno <marlonmoreno894@gmail.com>
2021-05-19 17:51:35 -0500

    commit tree ea855eb97b0c746832be5fdc79e6ab5eef94e328

--- index.js
+++ index.js
@@ -1,1 +1,55 @@
 // Your code here
+const mapToNegativize = function(array) {
+    let newArray = []
+    for (let i = 0; i < array.length; i++) {
+        newArray.push(array[i] * -1)
+    }
+    return newArray
+}
+
+const mapToNoChange = function(array) {
+    let newArray = []
+    for (let i = 0; i < array.length; i++) {
+        newArray.push(array[i])
+    }
+    return newArray
+}
+
+const mapToDouble = function(array) {
+    let newArray = []
+    for (let i = 0; i < array.length; i++) {
+        newArray.push(array[i] * 2)
+    }
+    return newArray
+}
+
+const mapToSquare = function(array) {
+    let newArray = []
+    for (let i = 0; i < array.length; i++) {
+        newArray.push(array[i] ** 2 )
+    }
+    return newArray
+}
+
+const reduceToTotal = function(array, initial = 0) {
+    let total = initial
+    for (let i = 0; i < array.length; i++) {
+        total += array[i]
+    }
+    return total
+}
+
+
+const reduceToAllTrue = function(array) {
+    for (let i = 0; i < array.length; i++) {
+        if (!array[i]) return false 
+    }
+    return true
+} 
+
+const reduceToAnyTrue = function(array) {
+    for (let i = 0; i < array.length; i++) {
+        if (array[i]) return true
+    }
+    return false
+} 
